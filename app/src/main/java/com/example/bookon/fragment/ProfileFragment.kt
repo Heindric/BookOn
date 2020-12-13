49ed2674ef6dce128ae3b9843e90ef029df6fe5f
AchmadFaisAlifAdityo <45361744+AchmadFaisAlifAdityo@users.noreply.github.com>
2020-12-13 18:41:31 +0700

    login dan register

--- app/src/main/java/com/example/bookon/fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/bookon/fragment/ProfileFragment.kt
@@ -1,11 +1,14 @@
 package com.example.bookon.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.bookon.Login
 import com.example.bookon.R
+import com.google.firebase.auth.FirebaseAuth
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -36,6 +39,7 @@ class ProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
+
     }
 
     companion object {
